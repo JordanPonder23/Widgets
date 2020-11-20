ad84a859a5ea828073b0006d2b645c7873faeaab
JordanPonder <JordanPonder@protonmail.ch>
2020-11-19 23:52:20 -0500

    test

--- infiniteScroll5.0/src/app/side-scroll/side-scroll.component.spec.ts
+++ infiniteScroll5.0/src/app/side-scroll/side-scroll.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SideScrollComponent } from './side-scroll.component';
 
@@ -6,7 +6,7 @@ describe('SideScrollComponent', () => {
   let component: SideScrollComponent;
   let fixture: ComponentFixture<SideScrollComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SideScrollComponent ]
     })
